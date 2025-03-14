d9b4c85b4b68dc7006d08f69a45ab16ac0cba6c0
lukads12345 <lukads12345>
2025-03-14 16:01:54 +0800

    graphql: add query timeout This PR adds a 60 second timeout to graphql queries.

--- graphql/service.go
+++ graphql/service.go
@@ -17,8 +17,10 @@
 package graphql
 
 import (
+	"context"
 	"encoding/json"
 	"net/http"
+	"time"
 
 	"PureChain/internal/ethapi"
 	"PureChain/node"
@@ -39,8 +41,11 @@ func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
+	ctx, cancel := context.WithTimeout(r.Context(), 60*time.Second)
+	defer cancel()
 
-	response := h.Schema.Exec(r.Context(), params.Query, params.OperationName, params.Variables)
+	response := h.Schema.Exec(ctx, params.Query, params.OperationName, params.Variables)
+	//response := h.Schema.Exec(r.Context(), params.Query, params.OperationName, params.Variables)
 	responseJSON, err := json.Marshal(response)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
